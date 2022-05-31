a5cbed272bf412323da09d819eef068386f64040
YaelPM <193259@ids.upchiapas.edu.mx>
2022-05-30 22:32:05 -0500

    Cambios chidos

--- src/comparadorlex/ComparadorLex.java
+++ src/comparadorlex/ComparadorLex.java
@@ -1,25 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package comparadorlex;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Luis Manuel
- */
 public class ComparadorLex extends Application {
-    
-   
     
     @Override
     public void start(Stage stage) throws Exception {
@@ -31,9 +18,6 @@ public class ComparadorLex extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
--- src/comparadorlex/homeController.java
+++ src/comparadorlex/homeController.java
@@ -1,5 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ * 
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -13,12 +13,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-/**
- *
- * @author Luis Manuel
- */
 public class homeController implements Initializable {
     
     Especificos especifico = new Especificos();
@@ -31,7 +28,14 @@ public class homeController implements Initializable {
     private Label labelTokenLetras;
     @FXML
     private Label labelnotFound;
- 
+    @FXML
+    private ListView<String> listViewEspecificos;
+    @FXML
+    private ListView<String> listViewLetras;
+    @FXML
+    private ListView<String> listViewNoEncontrado;
+    @FXML
+    private ListView<String> listViewTipo;
     @FXML
     private TextField parametro;
     ArrayList<String> palabra; 
@@ -39,36 +43,24 @@ public class homeController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         String[] word = parametro.getText().split("-");
-        String especificos="";
-        String tipo="";
-        String letras="";
-        String notFound ="";
         for (int i = 0; i < word.length; i++) {
             if (especifico.tokenSEspecificos(word[i])) {
-                especificos = especificos.concat(word[i]+"---");
-            labelTokenEspecific.setText(especificos+"--------se encontro en el token de especificos");
-        }else if (especifico.tokensTipo(word[i])){
-            tipo = tipo.concat(word[i]+"---");
-            labelTokentipo.setText(tipo+"--------se encontro en el token de tipo");
-        }else if(especifico.tokensLetras(word[i])){
-            letras = letras.concat(word[i]+"---");
-            labelTokenLetras.setText(letras+"--------se encontro en el token de letras");
-        }else{
-            notFound = notFound.concat(word[i]+"---");
-            labelnotFound.setStyle("-fx-background-color: orange;");
-           
-            labelnotFound.setText(notFound+"----No se encontro en el lexico");
+                listViewEspecificos.getItems().add(word[i]);
+            }else if (especifico.tokensTipo(word[i])){
+            	listViewTipo.getItems().add(word[i]);
+            }else if(especifico.tokensLetras(word[i])){
+            	listViewLetras.getItems().add(word[i]);
+            }else{
+            	listViewNoEncontrado.getItems().add(word[i]);
+            }
         }
-       }
-         
     }
     @FXML
     public void limpiarCampos(){
-        labelTokenEspecific.setText("Especificos");
-        labelTokentipo.setText("Tipo");
-        labelTokenLetras.setText("Letras");
-        labelnotFound.setText("No encontrados");
-        
+        listViewEspecificos.getItems().clear();
+        listViewLetras.getItems().clear();
+        listViewNoEncontrado.getItems().clear();
+        listViewTipo.getItems().clear();
     }
     
     @Override
